35aa1b4bdd40935ffa90d000fba794eafefb8a7c
German M. Bravo <german.mb@gmail.com>
2018-05-07 15:12:48 -0500

    Default headers support. Fixes #205

--- src/httpClient.ts
+++ src/httpClient.ts
@@ -81,9 +81,16 @@ export class HttpClient {
             options.headers = httpRequest.headers = {};
         }
 
-        // add default user agent if not specified
-        if (!hasHeader(options.headers, 'User-Agent')) {
-            options.headers['User-Agent'] = this._settings.defaultUserAgent;
+        // add default headers if not specified
+        for (let header in this._settings.defaultHeaders) {
+            if (!hasHeader(options.headers, header)) {
+                if (header !== 'Host' || httpRequest.url[0] === '/') {
+                    const value = this._settings.defaultHeaders[header];
+                    if (value) {
+                        options.headers[header] = value;
+                    }
+                }
+            }
         }
 
         let size = 0;
